d4882aebb8c7f4a05221d5b2c33ef909905905ec
Alfian <alfian@192.168.1.36>
2022-09-04 12:20:11 +0700

    init

---
+++ dashboard.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import plotly.express as px
+#from IPython.display import HTML
+from datetime import date
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash()
+
+#Get data
+today = date.today()
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRhcAWkWXIjp2XVAsnTLw13QGg6Ot9D_HBf_FMCA42qIWf034T8oKOgV6cTBJS29tfJRPHPyQ4DQJ6s/pub?gid=1978524058&single=true&output=csv")
+
+def phase(start, finish, phase):
+    df_phase = df[["Project Name", start, finish]]
+    df_phase = df_phase.dropna(subset=["Project Name"])
+    df_phase["Phase"] = phase
+    df_phase.rename(columns = {"Project Name":"Project", start:"Start", finish:"Finish"}, inplace=True)
+    #df_phase["Start"] = df_phase["Start"].fillna(today)
+    #df_phase["Finish"] = df_phase["Finish"].fillna(today)
+    df_phase["Start"] = pd.to_datetime(df_phase["Start"])
+    df_phase["Finish"] = pd.to_datetime(df_phase["Finish"])
+    return df_phase
+
+#Projects development phase
+df_development = phase(start="Date Leads Acquired", finish="Delegation Date", phase="Development")
+
+#Projects preparation phase
+df_preparation = phase(start="Delegation Date", finish="Main Activity Start Date", phase="Preparation")
+
+#Projects active phase
+df_active = phase(start="Main Activity Start Date", finish="Main Activity End Date", phase="Active")
+
+#Projects reporting phase
+df_reporting = phase(start="Main Activity End Date", finish="Final Report Submitted Date", phase="Reporting")
+
+#Projects closing phase
+df_closing = phase(start="Final Report Submitted Date", finish="Project Archived Date", phase="Closing")
+
+#convert to dataframe
+#df_development = pd.DataFrame(df_development)
+#df_preparation = pd.DataFrame(df_preparation)
+
+df_final = pd.concat(
+                [df_development, df_preparation, df_active, df_reporting, df_closing], 
+                ignore_index=True)
+
+colors = {}
+colors['Development'] =  'rgb(225, 225, 51)'
+colors['Preparation'] = 'rgb(76, 153, 0)'
+colors['Active'] = 'rgb(204, 102, 0)'
+colors['Reporting'] = 'rgb(0, 0, 204)'
+colors['Closing'] = 'rgb(51, 0, 102)'
+
+def fig_gantt():
+    fig = px.timeline(df_final, color="Phase", color_discrete_map = colors,
+                    x_start="Start", x_end="Finish", y="Project")
+
+    fig.update_layout(shapes=[
+        dict(
+        type='line',
+        yref='paper', y0=0, y1=1,
+        xref='x', x0=today, x1=today
+        )], autosize=False, width=1200, height=900)
+    return fig
+
+
+#HTML(fig.to_html()) #Change to comment while using Dash
+#fig.show()
+
+#Layout
+app.title = "SS Dashboard"
+app.layout = html.Div(
+                children = [
+                    html.H1(id = 'H1', 
+                        children = 'Prototype of SS Dashboard', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}), 
+                                            dcc.Graph(id = 'gantt', figure = fig_gantt())
+        ]
+    )
+
+if __name__ == '__main__': 
+    app.run_server()
